f860044e88610b3019abc8f83bbae0b5c8bb3028
ochronus <ochronus@ochronus.com>
2021-11-01 16:07:47 +0100

    x

--- go.mod
+++ go.mod
@@ -4,4 +4,10 @@ go 1.17
 
 require github.com/motemen/go-pocket v0.0.0-20201204003030-43b897100651
 
-require github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815 // indirect
+require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.6.1 // indirect
+	github.com/yuin/goldmark v1.4.2 // indirect
+)
--- main.go
+++ main.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	"github.com/motemen/go-pocket/api"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/ast"
+	"github.com/yuin/goldmark/text"
 )
 
-func main() {
-	consumerKey := getConsumerKey()
-
-	accessToken, err := restoreAccessToken(consumerKey)
-	if err != nil {
-		panic(err)
-	}
-
-	client := api.NewClient(consumerKey, accessToken.AccessToken)
+func getAllItems(client *api.Client, ai *authInfo) {
+	return
 	res, err := client.Retrieve(&api.RetrieveOption{
 		State:      "all",
 		Tag:        "newsletter-material",
@@ -30,14 +27,50 @@ func main() {
 			fmt.Println("\t", tag)
 		}
 
-		ai := &authInfo{
-			ConsumerKey: consumerKey,
-			AccessToken: accessToken.AccessToken,
-		}
 		res, err := AddTags(client, ai, item.ItemID, []string{"yolo"})
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(res)
+	}
+}
+
+func traverseAST(node ast.Node, entering bool) (ast.WalkStatus, error) {
+	if !entering {
+		if node.Kind() == ast.KindLink {
+			n := node.(*ast.Link)
+			fmt.Println(string(n.Destination))
+		}
+	}
+
+	return ast.WalkContinue, nil
+}
+
+func main() {
+	consumerKey := getConsumerKey()
+
+	accessToken, err := restoreAccessToken(consumerKey)
+	if err != nil {
+		panic(err)
+	}
+
+	client := api.NewClient(consumerKey, accessToken.AccessToken)
+
+	ai := &authInfo{
+		ConsumerKey: consumerKey,
+		AccessToken: accessToken.AccessToken,
+	}
+	getAllItems(client, ai)
+	fname := "..\\newsletter.leadership.garden\\content\\posts\\2021-09-10-waldeinsamkeit.md"
+	b, err := ioutil.ReadFile(fname)
+	if err != nil {
+		panic(err)
+	}
+
+	doc := goldmark.DefaultParser().Parse(text.NewReader(b))
+
+	err = ast.Walk(doc, traverseAST)
+	if err != nil {
+		panic(err)
 	}
 }
